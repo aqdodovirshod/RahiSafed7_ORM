2fa1f2a0d6facba1a5e8911a819d58714e2d118f
H4CKER <aqdodovirshod7@gmail.com>
2025-11-24 23:30:38 +0500

    solving administration error

--- server/settings.py
+++ server/settings.py
@@ -12,10 +12,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY') or 'django-insecure-dev-key-change-in-production-12345'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG_VAR = os.getenv('DEBUG_VAR')
+DEBUG = os.getenv('DEBUG_VAR', 'True').lower() in ['true', '1', 'yes']
 
 ALLOWED_HOSTS = ["*"]
 
